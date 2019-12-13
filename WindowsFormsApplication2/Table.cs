a207a857a8850f74733c19d01de8bb07ab29cdb7
steliospap <steliospap@bankingautomation.co.uk>
2019-12-13 12:20:25 +0000

    added classes for tha databases and the tables inside the databases

---
+++ WindowsFormsApplication2/Table.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Table : Database
+    {
+        //public variables
+        public string tableName;
+
+        //private variables
+        private List<string> fields = new List<string>(); //the field names of the table
+        private int tableRecordNumbers; //number of records each table has
+        
+        //========== public functions ============//
+
+        public Table()
+        {
+            tableName = "";
+            tableRecordNumbers = 0;
+        }
+        
+        public Table(string tableName)
+        {
+            this.tableName = tableName;
+        }
+
+        public void InitialiseTable(OleDbConnection conn)
+        {
+            ReadInTableRecords(conn);   //get the table records
+            ReadInTableFields(conn);    //get the table field names
+        }
+
+
+
+        //========== private functions ===========//
+
+        private void ReadInTableRecords(OleDbConnection conn) //gets the number of records in each table
+        {
+            OleDbCommand cmd = new OleDbCommand("select count(*) from " + tableName, conn);
+            tableRecordNumbers = (int) cmd.ExecuteScalar();
+        }
+
+        private void ReadInTableFields(OleDbConnection conn) //gets all the field names of the table
+        {
+            OleDbCommand cmd = new OleDbCommand("select * from " + tableName, conn);
+            OleDbDataReader reader = cmd.ExecuteReader(CommandBehavior.SchemaOnly);
+            {
+                DataTable table = reader.GetSchemaTable();
+                var nameCol = table.Columns["ColumnName"];
+                foreach (DataRow row in table.Rows)
+                {
+                    fields.Add(row[nameCol].ToString()); //adds the field name to the fields list
+                }
+            }
+        }
+
+    }
+}
